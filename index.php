d926af5a8e1ff43e79b7ef95146304e9f097e455
Antonio Iago Lemos Gaspar <iagogaspar.uema.dpd@gmail.com>
2016-09-13 14:29:59 -0300

    Adicionando cookies e refatorando a lógica do usuário.

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php include("cabecalho.php"); ?>
+<?php include("cabecalho.php");
+include("logica-usuario.php");
+?>
 	
 	<?php if(isset($_GET['login']) && $_GET['login']==true): ?>
 		<p class="alert-success">Logado com sucesso!</p>
@@ -8,22 +10,30 @@
 		<p class="alert-danger">Usuário ou senha inválida!</p>
 	<?php endif; ?>
 
+	<?php if(isset($_GET['falhaDeSeguranca']) && $_GET['falhaDeSeguranca']==true): ?>
+		<p class="alert-danger">Você não tem acesso a essa funcionalidade!</p>
+	<?php endif; ?>
+
 	<h1>Bem vindo!</h1>
 
-	<h2>Login</h2>
-	<form action="login.php" method="post">
-		<table class="table">
-			<tr>
-				<td>Email</td>
-				<td><input class="form-control" type="email" name="email"></td>
-			</tr>
-			<tr>
-				<td>Senha</td>
-				<td><input class="form-control" type="password" name="senha"></td>
-			</tr>
-			<tr>
-				<td><button type="submit" class="btn btn-primary">Login</button></td>
-			</tr>
-		</table>
-	</form>
+	<?php if(usuarioEstaLogado()): ?>
+		<p class="text-success">Você está logado como <?=usuarioLogado();?>.</p>
+	<?php else: ?>
+		<h2>Login</h2>
+		<form action="login.php" method="post">
+			<table class="table">
+				<tr>
+					<td>Email</td>
+					<td><input class="form-control" type="email" name="email"></td>
+				</tr>
+				<tr>
+					<td>Senha</td>
+					<td><input class="form-control" type="password" name="senha"></td>
+				</tr>
+				<tr>
+					<td><button type="submit" class="btn btn-primary">Login</button></td>
+				</tr>
+			</table>
+		</form>
+	<?php endif; ?>
 <?php include("rodape.php"); ?>			
---
+++ logica-usuario.php
@@ -0,0 +1,21 @@
+<?php
+
+function usuarioEstaLogado() {
+	return isset($_COOKIE['usuario_logado']);
+}
+
+function verificaUsuario() {
+	if(!usuarioEstaLogado()) {
+		header("Location: index.php?falhaDeSeguranca=true");
+		die();
+	}
+}
+
+function usuarioLogado() {
+	return $_COOKIE['usuario_logado'];
+}
+
+function logaUsuario($usuario) {
+	setcookie("usuario_logado", $usuario, time() + 60);
+}
+?>
